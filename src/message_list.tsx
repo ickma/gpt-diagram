07e55d0e71a7c6d8b76bc4a5ceea2f8344dfbf4c
Chao Ma <chao.ma@li.me>
2023-06-29 08:17:41 +0800

    feat: Refactor message list component and update dependencies
    - Add `@mui/material` as a dependency with version `^5.13.6`
    - Added a new file `src/messages_list.stories.tsx`
    - Refactored and improved `src/message_list.tsx`
    - Changed the appearance of messages based on sender (AI or user)
    - Added timestamps to messages

--- src/message_list.tsx
+++ src/message_list.tsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import {  MessageList  as MessageListComponent, MessageType } from "react-chat-elements" 
+import * as React from 'react';
+import { Box, Typography } from '@mui/material';
 
-type Message ={
-  position: 'left' | 'right';
-  type: 'text';
-  text: string;
-  title: string;
-  date: Date
+interface IMessage {
+  sender: string;
+  message: string | React.ReactNode;
+  timestamp: number; // Unix timestamp (in seconds)
 }
 
-const MessageList = (props: {messages: Message[]}) => {
+interface IChatProps {
+  messages: IMessage[];
+}
+
+const getTimeString = (timestamp: number): string => {
+  const date = new Date(timestamp * 1000); // Convert from seconds to milliseconds
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+const MessageList: React.FC<IChatProps> = (props: IChatProps) => {
   const { messages } = props;
+
   return (
-    <MessageListComponent
-      className='message-list'
-      lockable={true}
-      referance={null}
-      toBottomHeight={'100%'}
-      dataSource={messages as MessageType []}
-    />
-  )
-}
+    <Box sx={{ overflowY: 'auto', minHeight: '200px' }}>
+      {messages.map((message: IMessage, index: number) => {
+        const isAi = message.sender === 'AI';
+        const textAlign = isAi ? 'left' : 'right';
+        const color = isAi ? 'primary.main' : 'secondary.main';
+        const backgroundColor = isAi ? '#E0E0E0' : '#DCF8C6';
+        
+        return (
+          <Box
+            key={index}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: textAlign,
+              my: 1,
+              mx: isAi ? 1 : 0,
+              px: 1,
+              py: 0.5,
+              borderRadius: '10px',
+              backgroundColor,
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              fontWeight="bold"
+              color={color}
+              sx={{ mb: 0.5 }}
+            >
+              {message.sender} ({getTimeString(message.timestamp)}):
+            </Typography>
+            <Typography variant="body2">{message.message}</Typography>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default MessageList;
